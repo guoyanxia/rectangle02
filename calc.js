8f27cadac6094cf9c826211e7f7557b16dd223b3
wangding <408542507@qq.com>
2019-03-27 16:29:40 +0800

    数据合法性校验

---
+++ calc.js
@@ -0,0 +1,79 @@
+function Rectangle(width, height) {
+
+    var w = Number(width),
+
+              h = Number(height);
+
+
+
+    this.area = function() {
+
+          return w * h;
+
+            
+    };
+
+
+
+    this.perimeter = function() {
+
+          return 2 * (w + h);
+
+            
+    };
+
+
+}
+
+function validate(data) {
+
+  var result = {
+
+        isOK: false,
+
+            reason: ''
+
+                
+  };
+
+
+
+  if(data === '') {
+
+        result.reason = '不能为空！';
+
+            return result;
+
+              
+  }
+
+
+
+  if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(data)) {
+
+        result.reason = '必须是数值';
+
+            return result;
+
+              
+  }
+
+
+
+  if(Number(data) < 0) {
+
+        result.reason = '必须大于零';
+
+            return result;
+
+              
+  }
+
+
+
+    result.isOK = true;
+
+      return result;
+
+
+}
