a977295f5895f76a038a289f2d24ac513a379cab
wangding <408542507@qq.com>
2019-04-02 16:52:55 +0800

    集中校验

--- rectangle.js
+++ rectangle.js
@@ -15,26 +15,6 @@ $(function() {
       $heightValidate = $('#height-validate'),
 
       isPassValidate = false;
-$width.focusout(function() {
-      var result = validate($width.val());
-      isPassValidate = result.isOK;
-      if(!result.isOK) {
-          $widthValidate.html('宽度' + result.reason);
-          $width.select();                    
-          } else {
-               $widthValidate.html('');                      
-          }            
-});
-$height.focusout(function() {
-      var result = validate($height.val());
-          isPassValidate = result.isOK;
-          if(!result.isOK) {
-                  $heightValidate.html('高度' + result.reason);
-                        $height.select();                            
-          } else {
-                 $heightValidate.html('');                      
-          }            
-});
 $btnCal.click(function(){
       if(!isPassValidate) return;
           var w = $width.val(),
